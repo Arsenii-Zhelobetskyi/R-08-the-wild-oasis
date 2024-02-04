7b5e36ffbca4fb91eb1b41237bd41db5a9b89c50
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2024-02-04 22:57:39 +0200

    added dark-mode and user-changing-credential feature

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 export async function signUp({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
     email,
@@ -35,4 +35,34 @@ export async function logout() {
   const { error } = await supabase.auth.signOut(); //clear the local storage
 
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. Update password or fullName
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData); // automatically updates the current user in supabase
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // 2. Upload the avatar image to the bucket
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+  if (storageError) throw new Error(storageError.message);
+
+  // 3. Update avatar in the user
+  const { data: updatedUser, error: avatarError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+  if (avatarError) throw new Error(avatarError.message);
+  return updatedUser;
 }
